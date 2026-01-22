01e9570ca0c2c0c21d3f1555248ad1f7384004dc
Himjoshi21 <joshihimanshu9295@gmail.com>
2026-01-23 01:02:29 +0530

    add Student and BankAccount classes with methods for adult check, urban area identification, and account operations

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,12 @@
-d={1:"a",2:"b",3:"c"}
-# print(d.keys()) #it will print all the keys present in the dictionary
-# print(d.values())#it will print all the values present in the dictionary
-# print(d.items())#it will print all the key value pair present in the dictionary
-# for i in d.keys():
-#     print(d[i])
-for i in d.items():
-    print(i[0]) 
-# for i in d.values():
-#     print(i)   
+class employee:
+    def __init__(self,employee_id,name,basic_salary):
+        self.employee_id=employee_id
+        self.name=name
+        self.basic_salary=basic_salary
+    def calaculate_salary(self):
+        hra=0.2*self.basic_salary
+        da=0.1*self.basic_salary
+        total_salary=self.basic_salary+hra+da
+        return total_salary
+himanshu=employee(101,"Himanshu",50000)
+print(himanshu.calaculate_salary()) 
