001d1c5fc8669cd5e5a8636dbcf0da43aa528bee
Himjoshi21 <joshihimanshu9295@gmail.com>
2026-01-25 17:26:34 +0530

    add isSubsequence and majorityElement implementations with example cases

---
+++ Himanshuleetcode-questions/majorityElement.py
@@ -0,0 +1,44 @@
+# Given an array nums of size n, return the majority element.
+
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+# Example 1:
+
+# Input: nums = [3,2,3]
+# Output: 3
+# Example 2:
+
+# Input: nums = [2,2,1,1,1,2,2]
+# Output: 2
+ 
+
+# Constraints:
+
+# n == nums.length
+# 1 <= n <= 5 * 104
+# -109 <= nums[i] <= 109
+# The input is generated such that a majority element will exist in the array.
+ 
+
+# Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+
+class Solution:
+    def majorityElement(nums):
+        cnt=0
+        res=nums[0]
+        for i in range(len(nums)):
+            if cnt==0:
+              res=nums[i]
+            if nums[i]==res:
+                cnt+=1
+            else:
+                cnt-=1
+        return res
+    nums = [3,2,3]
+    print(majorityElement(nums))
+        
+            
+
